385284edf8cbf19599b54d001b4f4ca129f30d91
Luis Fernando <xvleg3ndaryxv@gmail.com>
2018-01-03 17:02:42 -0600

    Proyect base

--- JSONRequest/ViewController.swift
+++ JSONRequest/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
+
+struct Pokemon : Decodable {
+    var id : Int
+    var name : String
+    var weight : Int
+    var base_experience : Int
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        let urlString = "https://pokeapi.co/api/v2/pokemon/1/"
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data,response, error) in
+            //Handle error
+            //Handle response
+            print("Do stuff here")
+            guard let data = data else { return }
+            
+            //let stringAnswer = String(data: data, encoding: .utf8)
+            let pokemon = try! JSONDecoder().decode(Pokemon.self, from: data)
+                print(pokemon.name)
+            
+        }.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
